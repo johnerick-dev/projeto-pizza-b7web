2adb70b960259dee2892c249fbaadb2d96097cfd
John Erick <johnerick.dev@outlook.com>
2022-05-31 21:22:33 -0300

    Evento adicionar no carrinho

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+let cart = [];
 let modalQt = 1;
+let modalKey = 0;
 
 const c = (el) => {
     return document.querySelector(el);
@@ -18,6 +20,7 @@ pizzaJson.map((item, index) => {
         e.preventDefault();
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
         modalQt = 1;
+        modalKey = key;
 
         c('.pizzaBig img').src = pizzaJson[key].img;
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
@@ -72,4 +75,15 @@ cs('.pizzaInfo--size').forEach((size, sizeIndex) =>{
         size.classList.add('selected');
     });
 });
+//Evento adicionar no carrinho
+c('.pizzaInfo--addButton').addEventListener('click', ()=>{
+    let size = parseInt(c('.pizzaInfo--size.selected').getAttribute('data-key'));
 
+    cart.push({
+        id:pizzaJson[modalKey].id,
+        size:size,
+        qt:modalQt
+    });
+
+    closeModal();
+});
